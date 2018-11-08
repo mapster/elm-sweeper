7eec56d0486f637f2a9bc2e1892bc5755367b1e6
Alexander Hoem Rosbach <alexander@rosbach.no>
2018-11-08 23:08:54 +0100

    Format using elm-format

--- src/Main.elm
+++ src/Main.elm
@@ -6,84 +6,113 @@ import Minefield exposing (Cell, Content(..), Minefield, isFresh, isMine)
 import Random
 
 
-main = Browser.document
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 type alias Model =
-    { state: GameState
-    , field: Minefield
+    { state : GameState
+    , field : Minefield
     }
+
 
 type GameState
     = Playing
     | GameOver
 
-init : () -> (Model, Cmd Msg)
+
+init : () -> ( Model, Cmd Msg )
 init _ =
-    (Model Playing <| Minefield.init 10, Cmd.none)
+    ( Model Playing <| Minefield.init 10, Cmd.none )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.none
 
 
+
 -- Update
+
 
 type Msg
     = ClickCell Cell
     | InitCell Cell Float
     | NewGame
 
-update : Msg -> Model -> (Model, Cmd Msg)
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         ClickCell cell ->
             let
-                updCell = updateCell cell
-                minefield = Minefield.replace updCell model.field
-                cmd = Minefield.rows minefield
+                updCell =
+                    updateCell cell
+
+                minefield =
+                    Minefield.replace updCell model.field
+
+                cmd =
+                    Minefield.rows minefield
                         |> List.foldl (++) []
                         |> List.filter isFresh
                         |> List.map (\c -> Random.generate (InitCell c) (Random.float 0 1))
                         |> Cmd.batch
-                state = if isMine cell then GameOver else model.state
+
+                state =
+                    if isMine cell then
+                        GameOver
+
+                    else
+                        model.state
             in
-                ({model | field = minefield, state = state}, cmd)
+            ( { model | field = minefield, state = state }, cmd )
 
         InitCell cell rnJesus ->
             let
-                updCell = {cell | content = Hidden (rnJesus <= 0.37)}
+                updCell =
+                    { cell | content = Hidden (rnJesus <= 0.37) }
             in
-                ({model | field = Minefield.replace updCell model.field}, Cmd.none)
+            ( { model | field = Minefield.replace updCell model.field }, Cmd.none )
 
         NewGame ->
             init ()
 
+
 updateCell cell =
     case cell.content of
-        Fresh -> {cell | content = Visible False}
-        Hidden hasMine -> {cell | content = Visible hasMine}
-        Visible _ -> cell
+        Fresh ->
+            { cell | content = Visible False }
+
+        Hidden hasMine ->
+            { cell | content = Visible hasMine }
+
+        Visible _ ->
+            cell
+
+
 
 -- View
+
+
 view : Model -> Browser.Document Msg
 view model =
     { title = "Elm-sweeper"
-    , body = 
-        [
-            div []
-                [ h1 [] [ text "Elm-sweeper" ]
-                , button [ onClick NewGame] [ text "New Game"]
-                , viewField model.field
-                , viewState model.state
-                ]
+    , body =
+        [ div []
+            [ h1 [] [ text "Elm-sweeper" ]
+            , button [ onClick NewGame ] [ text "New Game" ]
+            , viewField model.field
+            , viewState model.state
+            ]
         ]
     }
+
 
 viewState state =
     case state of
@@ -93,17 +122,23 @@ viewState state =
         GameOver ->
             h2 [] [ text "Game Over!" ]
 
+
 viewField field =
     div [] (List.map (viewRow field) <| Minefield.rows field)
 
+
 viewRow field row =
     div [ class "row" ] (List.map (viewCell field) row)
+
 
 viewCell field cell =
     case cell.content of
         Visible hasMine ->
             if hasMine then
                 div [ class "cell mine" ] []
+
             else
                 div [ class "cell" ] [ text <| String.fromInt <| Minefield.adjacent cell field ]
-        _ -> div [ class "cell hidden", onClick <| ClickCell cell] []
+
+        _ ->
+            div [ class "cell hidden", onClick <| ClickCell cell ] []
--- src/Minefield.elm
+++ src/Minefield.elm
@@ -1,58 +1,81 @@
-module Minefield exposing (..)
+module Minefield exposing (Cell, Content(..), Minefield, adjacent, adjacentRel, get, init, isFresh, isMine, replace, rows)
 
 import Array exposing (Array)
 import Maybe exposing (andThen)
-adjacentRel = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
 
-type alias Minefield = Array (Array Cell)
+
+adjacentRel =
+    [ ( -1, -1 ), ( -1, 0 ), ( -1, 1 ), ( 0, -1 ), ( 0, 1 ), ( 1, -1 ), ( 1, 0 ), ( 1, 1 ) ]
+
+
+type alias Minefield =
+    Array (Array Cell)
+
 
 type alias Cell =
-    { content: Content
-    , row: Int
-    , col: Int
+    { content : Content
+    , row : Int
+    , col : Int
     }
+
 
 type Content
     = Fresh
     | Hidden Bool
     | Visible Bool
 
+
 init dim =
     Array.initialize dim (Cell Fresh)
         |> Array.map (Array.initialize dim)
+
 
 rows field =
     Array.map Array.toList field
         |> Array.toList
 
+
 replace cell field =
     case Array.get cell.row field of
         Just row ->
             let
-                updRow = Array.set cell.col cell row
+                updRow =
+                    Array.set cell.col cell row
             in
-                Array.set cell.row updRow field
+            Array.set cell.row updRow field
 
         Nothing ->
             field
 
+
 isFresh cell =
     case cell.content of
-        Fresh -> True
-        _ -> False
+        Fresh ->
+            True
+
+        _ ->
+            False
+
 
 isMine cell =
     case cell.content of
-        Fresh -> False
-        Hidden hasMine -> hasMine
-        Visible hasMine -> hasMine
+        Fresh ->
+            False
 
-get field (r, c) =
+        Hidden hasMine ->
+            hasMine
+
+        Visible hasMine ->
+            hasMine
+
+
+get field ( r, c ) =
     Array.get r field
         |> andThen (Array.get c)
 
+
 adjacent cell field =
-    List.map (\(r, c) -> (cell.row + r, cell.col + c)) adjacentRel
+    List.map (\( r, c ) -> ( cell.row + r, cell.col + c )) adjacentRel
         |> List.filterMap (get field)
         |> List.filter isMine
         |> List.length
